ed618fee73f2911ec34cfb527e896488eb0af96b
Darren Forsythe <forsythed90@gmail.com>
2017-06-11 19:22:06 +0100

    unsued imports

--- src/main/java/com/darrenforsythe/mixinabstractclass/MixinabstractclassApplication.java
+++ src/main/java/com/darrenforsythe/mixinabstractclass/MixinabstractclassApplication.java
@@ -2,11 +2,6 @@ package com.darrenforsythe.mixinabstractclass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
-import com.darrenforsythe.mixinabstractclass.config.AnimalMixin;
-import com.darrenforsythe.mixinabstractclass.model.Animal;
 
 @SpringBootApplication
 public class MixinabstractclassApplication {
